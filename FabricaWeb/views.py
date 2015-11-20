5ba384e10962fcc4d86e3e6d84055891515bfc0d
Benjamin Perez <josebenjaminp@gmail.com>
2015-11-20 15:14:24 -0400

    me olvide la funcion en views.py

--- FabricaWeb/views.py
+++ FabricaWeb/views.py
@@ -1,5 +1,8 @@
 
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 
 def index(request):
     return HttpResponseRedirect('/station/')
+
+def services(request):
+    return HttpResponse('prueba')
