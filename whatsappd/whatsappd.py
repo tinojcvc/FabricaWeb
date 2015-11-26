f69b6d0b9a481a6e07bf5b441b4806306d4a30f8
Benjamin Perez <josebenjaminp@gmail.com>
2015-11-26 17:50:30 -0400

    concatenacion de mensajes v1

--- whatsappd/whatsappd.py
+++ whatsappd/whatsappd.py
@@ -1,8 +1,19 @@
 from wasend import YowsupSendStack
 from wareceive import YowsupReceiveStack, MessageReceived
 
+from mongoengine import *
+
+import datetime
+
+connect('fabricaweb')
+
+class WhatsappReceived(Document):
+    phone = StringField()
+    message = StringField()
+    date = DateTimeField()
+
 def credential():
-     return "59100000000", "password"
+     return "59167479531", "kgWT7zbZhFxGEl/fJoVGwn/Kzbw="
 
 while True:
     try:
@@ -17,4 +28,46 @@ while True:
         print 'Phone: ' + phone
         print 'Message: ' + message
         print '------------------------'
+
+        dbphone = WhatsappReceived.objects.filter(phone=phone).order_by('-date')
+        len_dbphone = len(dbphone)
+        if len_dbphone > 0:
+            print '------------------'
+            print 'El telefono existe'
+            print '------------------'
+            now = datetime.datetime.now()
+            if dbphone[0].date.date() == now.date():
+                time = dbphone[0].date + datetime.timedelta(minutes=30)
+                if now < time:
+                    last_message = dbphone[0].message
+                    print '-----------------'
+                    print dbphone[0].date.date()
+                    print 'mensaje anterior: ' + last_message
+                    print 'mensaje ultimo: ' + message
+                    print '-----------------'
+                    try:
+                        current_message = last_message + ' ' + message
+                        dbphone[0].update(message=current_message)
+                    except Exception as e:
+                        print '******************'
+                        print e
+                        print '******************'
+                else:
+                    print '------------------'
+                    print 'nuevo mensaje del numero: ' + phone
+                    print '------------------'
+                    whatsapp = WhatsappReceived(phone=phone,
+                                                message=message,
+                                                date=datetime.datetime.now())
+                    whatsapp.save()
+            else:
+                whatsapp = WhatsappReceived(phone=phone,
+                                            message=message,
+                                            date=datetime.datetime.now())
+                whatsapp.save()
+        else:
+            whatsapp = WhatsappReceived(phone=phone,
+                                        message=message,
+                                        date=datetime.datetime.now())
+            whatsapp.save()
 
