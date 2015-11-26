52f35806258789d46cd3ac2e9a7330705690281f
Benjamin Perez <josebenjaminp@gmail.com>
2015-11-26 18:28:00 -0400

    conexion whatsapp version2

--- whatsappd/whatsappd.py
+++ whatsappd/whatsappd.py
@@ -1,3 +1,4 @@
+
 from wasend import YowsupSendStack
 from wareceive import YowsupReceiveStack, MessageReceived
 
@@ -8,12 +9,17 @@ import datetime
 connect('fabricaweb')
 
 class WhatsappReceived(Document):
-    phone = StringField()
-    message = StringField()
-    date = DateTimeField()
+    phone = StringField(required=True)
+    message = StringField(required=False)
+    image = StringField(required=True)
+    audio = BinaryField(required=False)
+    video = BinaryField(required=False)
+    is_valid = BooleanField(required=True)
+    location = StringField(required=False)
+    date_creation = DateTimeField(required=True)
 
 def credential():
-     return "59167479531", "kgWT7zbZhFxGEl/fJoVGwn/Kzbw="
+     return "59100000000", "password"
 
 while True:
     try:
@@ -29,45 +35,39 @@ while True:
         print 'Message: ' + message
         print '------------------------'
 
-        dbphone = WhatsappReceived.objects.filter(phone=phone).order_by('-date')
+        dbphone = WhatsappReceived.objects.filter(phone=phone).order_by('-date_creation')
         len_dbphone = len(dbphone)
         if len_dbphone > 0:
             print '------------------'
             print 'El telefono existe'
             print '------------------'
             now = datetime.datetime.now()
-            if dbphone[0].date.date() == now.date():
-                time = dbphone[0].date + datetime.timedelta(minutes=30)
-                if now < time:
-                    last_message = dbphone[0].message
-                    print '-----------------'
-                    print dbphone[0].date.date()
-                    print 'mensaje anterior: ' + last_message
-                    print 'mensaje ultimo: ' + message
-                    print '-----------------'
-                    try:
-                        current_message = last_message + ' ' + message
-                        dbphone[0].update(message=current_message)
-                    except Exception as e:
-                        print '******************'
-                        print e
-                        print '******************'
-                else:
-                    print '------------------'
-                    print 'nuevo mensaje del numero: ' + phone
-                    print '------------------'
-                    whatsapp = WhatsappReceived(phone=phone,
-                                                message=message,
-                                                date=datetime.datetime.now())
-                    whatsapp.save()
+            if dbphone[0].date_creation.date() == now.date() and now < (dbphone[0].date_creation + datetime.timedelta(minutes=30)):
+                last_message = dbphone[0].message
+                print '-----------------'
+                print dbphone[0].date_creation.date()
+                print 'mensaje anterior: ' + last_message
+                print 'mensaje ultimo: ' + message
+                print '-----------------'
+                try:
+                    current_message = last_message + ' ' + message
+                    dbphone[0].update(message=current_message)
+                except Exception as e:
+                    print '******************'
+                    print e
+                    print '******************'
             else:
                 whatsapp = WhatsappReceived(phone=phone,
                                             message=message,
-                                            date=datetime.datetime.now())
+                                            image='asdf',
+                                            is_valid=True,
+                                            date_creation=datetime.datetime.now())
                 whatsapp.save()
         else:
             whatsapp = WhatsappReceived(phone=phone,
                                         message=message,
-                                        date=datetime.datetime.now())
+                                        image='asdf',
+                                        is_valid=True,
+                                        date_creation=datetime.datetime.now())
             whatsapp.save()
 
