5f6ece3e07174a352c477a3eaeda619a2971a2af
irvin373 <irvin373@gmail.com>
2015-11-28 16:29:57 -0400

    base64 decode view

--- whatsapp/models.py
+++ whatsapp/models.py
@@ -1,5 +1,9 @@
 
 from mongoengine import *
+from cStringIO import StringIO
+import cStringIO
+from PIL import Image
+import base64
 
 connect('fabricaweb')
 
@@ -14,3 +18,13 @@ class WhatsappReceived(Document):
     is_read = BooleanField(required=True, default=False)
     location = StringField(required=False)
     date_creation = DateTimeField(required=True)
+
+
+    def hola(self):
+        data = StringIO(self.image)
+        img = Image.open(data)
+        resizeImg = img.resize((150,150), Image.ANTIALIAS)
+        buffer = cStringIO.StringIO()
+        resizeImg.save(buffer, format="JPEG")
+        img_str = base64.b64encode(buffer.getvalue())
+        return img_str
