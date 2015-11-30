07a155c1f42d99457a22ac26415dab3b65199226
Jhoel <jhoel.villarroel@gmail.com>
2015-11-30 17:00:00 -0400

    Se aumento la parte de mostrar los mensajes de Whatsapp y la parte de modificar el is_valid y el is_read

--- whatsapp/views.py
+++ whatsapp/views.py
@@ -12,7 +12,7 @@ from django.utils.six import BytesIO
 
 # Create your views here.
 def index(request):
-    list_msg = WhatsappReceived.objects
+    list_msg = WhatsappReceived.objects.filter(is_read=False)
     return render_to_response('whats_online.html', {'list_msg':list_msg},
                               context_instance=RequestContext(request))
 
@@ -20,8 +20,34 @@ def index(request):
      #                         context_instance=RequestContext(request))
 
 def view_message(request, message_id):
-    list_content = WhatsappReceived.objects.filter(phone=message_id)
+    message = WhatsappReceived.objects.get(id=message_id)
+    message.update(is_read=True)
+
+    list_content = WhatsappReceived.objects.filter(id=message_id)
     #phone_number = WhatsappReceived.objects.get(id=message_id)
 
     return render_to_response('view_message.html', {'list_content': list_content},
+                              context_instance=RequestContext(request))
+
+def no_valid(request, message_id):
+    message = WhatsappReceived.objects.get(id=message_id)
+    message.update(is_valid=False)
+
+    list_msg = WhatsappReceived.objects.filter(is_read=False)
+    return render_to_response('whats_online.html', {'list_msg':list_msg},
+                              context_instance=RequestContext(request))
+
+def view_all(request):
+    list_msg = WhatsappReceived.objects.filter
+    return render_to_response('whats_online.html', {'list_msg':list_msg},
+                              context_instance=RequestContext(request))
+
+def view_read(request):
+    list_msg = WhatsappReceived.objects.filter(is_read=True)
+    return render_to_response('whats_online.html', {'list_msg':list_msg},
+                              context_instance=RequestContext(request))
+
+def view_no_valid(request):
+    list_msg = WhatsappReceived.objects.filter(is_valid=False)
+    return render_to_response('whats_online.html', {'list_msg':list_msg},
                               context_instance=RequestContext(request))
