65c2e6b2b9bbf2b268fb2e56c43e3820ca66b9e7
Benjamin Perez <josebenjaminp@gmail.com>
2015-12-05 12:20:46 -0400

    paginacion el la vista de todos los mensajes

--- whatsapp/views.py
+++ whatsapp/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, render_to_response
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.template import RequestContext
 
 from whatsapp.models import WhatsappReceived
 
 def index(request):
     list_msg = WhatsappReceived.objects.filter(is_read=False).order_by('-date_creation')
-    return render(request, 'whats_online.html', {'list_msg':list_msg})
+    return render(request, 'whats_online.html', {'list_msg':list_msg, 'size': len(list_msg)})
 
     #return render_to_response('whatsapp.html', {'phones': phones},
      #                         context_instance=RequestContext(request))
@@ -30,10 +32,23 @@ def is_valid(request, message_id):
 
 def view_all(request):
     list_msg = WhatsappReceived.objects.order_by('-date_creation')
-    return render(request, 'whats_online_all.html', {'list_msg':list_msg})
+    paginator = Paginator(list_msg, 12) # Show 25 contacts per page
+
+    page = request.GET.get('page')
+    try:
+        messages = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        messages = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        messages = paginator.page(paginator.num_pages)
+
+    return render_to_response('whats_online_all.html', {"messages": messages})
+#    return render(request, 'whats_online_all.html', {'list_msg':list_msg})
 
 def view_read(request):
-    list_msg = WhatsappReceived.objects.filter(is_read=True).order_by('-date_creation')
+    list_msg = WhatsappReceived.objects.filter(is_read=True).filter(is_valid=True).order_by('-date_creation')
     return render(request, 'whats_online_read.html', {'list_msg':list_msg})
 
 def view_no_valid(request):
