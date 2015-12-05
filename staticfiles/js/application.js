82f5844bd99b88d44baee3276f01eb0346001d6e
irvin373 <irvin373@gmail.com>
2015-12-05 13:45:01 -0400

    Frond End final 0.1

--- staticfiles/js/application.js
+++ staticfiles/js/application.js
@@ -7,8 +7,6 @@
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
-$('.datepicker').pickadate({
-    selectMonths: true, // Creates a dropdown to control month
-    selectYears: 15 // Creates a dropdown of 15 years to control year
-});
-
+$(document).ready(function(){
+      $('.parallax').parallax();
+    });
