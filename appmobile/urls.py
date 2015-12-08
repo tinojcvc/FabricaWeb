9278838b9e4fd6cede5c7b0feaeb4b60eafeaef4
Benjamin Perez <josebenjaminp@gmail.com>
2015-12-08 17:33:14 -0400

    urls para appmobile

--- appmobile/urls.py
+++ appmobile/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from appmobile import views
 
 urlpatterns = [
+    url(r'^$', views.index, name='appmobile'),
     url(r'^services/$', views.services, name='app_sevices'),
 ]
 
