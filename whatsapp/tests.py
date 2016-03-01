bcc2596c1a32dca400e79d07eaea6162ff7d6f0d
Benjamin Perez <josebenjaminp@gmail.com>
2016-03-01 17:08:53 -0400

    tests en los views

--- whatsapp/tests.py
+++ whatsapp/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
-# Create your tests here.
+class TestCalls(TestCase):
+    def test_call_view_denies_anonymous(self):
+        response = self.client.get('/whatsapp/', follow=True)
+        self.assertRedirects(response, '/accounts/sigin/')
+        response = self.client.post('/appmobile/', follow=True)
+        self.assertRedirects(response, '/accounts/sigin/')
